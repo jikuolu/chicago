129ac33ef5a6bff83aa05810665e675f671f7b69
jikuolu <jikuolu@gmail.com>
2016-09-30 21:37:11 -0400

    fix graph

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ Created on Thu Sep  1 21:49:46 2016
 @author: Jikuo
 """
 
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect, send_from_directory
 from stockgraph import stock_graph
 app_stock = Flask(__name__)
 
@@ -16,6 +16,10 @@ def main():
 @app_stock.route('/index', methods=['GET','POST'])
 def index():
     return render_template('index.html')
+
+@app_stock.route('/images/<path:path>')
+def send_images(path):
+    return send_from_directory('images', path)
 
 if __name__ == "__main__":
     app_stock.run(debug=True)
